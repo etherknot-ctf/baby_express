e46419e808fb3c6a7364e3dd6c72f0febe83f44f
196Ikuchil <196thinline@gmail.com>
2021-08-26 20:20:31 +0900

    feat:get & post

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('ejs', ejs.renderFile);
 
-app.get('/', (req, res) => {
-          res.send('Hello World!')
+
+function test(req, res) {
+  res.send('Hello World!')
+}
+
+
+app.get('/', test)
+
+app.get('/form.html',(req,res)=> {
+	res.sendFile(__dirname + '/form.html')
 })
 
 app.get('/test.html',(req,res)=> {
 	res.sendFile(__dirname + '/test.html')
 })
+
+app.get('/target1', (req, res) => {
+  res.send('Hello' + req.query.namehoge);
+});
+
+app.post('/target1', (req, res) => {
+  res.send('Hello' + req.body.namefuga);
+});
+
+app.post('/target2', (req, res) => {
+  res.render(__dirname + '/target2.ejs', { gggg: req.body.gggg});
+});
 
 app.listen(port, () => {
           console.log(`Example app listening at http://localhost:${port}`)
